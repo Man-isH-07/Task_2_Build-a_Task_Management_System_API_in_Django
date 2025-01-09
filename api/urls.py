b140e3ec61782e0bb824b26a931a7423ca8ba32e
Man-isH-07 <manishdhaye07@gmail.com>
2025-01-09 15:53:37 +0530

    Done

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from task import views
 urlpatterns = [
-    path('get-task/',views.get_task),
-    path('tasks/',views.TaskAPI.as_view())
+    path('home/',views.home),
+    # path('tasks/',views.TaskAPI.as_view())
 ]
--- task/views.py
+++ task/views.py
@@ -1,40 +1,98 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
+from django.http import HttpResponse
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
-# Create your views here.
-
+# # Create your views here.
 @api_view(['GET','POST','DELETE'])
-def get_task(request):
+def get(request):
+    queryset = Task.objects.all().order_by('-pk')  # Fetch all records, ordered by descending primary key
+    serializer = TaskSerializer(queryset, many=True)  # Serialize all tasks
+    return Response(serializer.data) 
+
+def home(request):
+    if request.method=="POST":
+        data = request.POST
+        title = data.get("title")
+        description = data.get("description")
+        task_type = data.get("task_type")
+
+        print(data)
+
+        Task.objects.create(
+            title=title,
+            description=description,
+            task_type=task_type,
+        )
+
+        return redirect('/home')
+
     queryset = Task.objects.all()
-    serializer = TaskSerializer(queryset,many=True)
-
-    return Response( serializer.data)
-
-class TaskAPI(APIView):
-    def get(self,request):
-        queryset = Task.objects.all().order_by('-pk')
-        serializer = TaskSerializer(queryset,many=True)
-
-        return Response( serializer.data)
+    context = {'tasks' : queryset}
     
-    def post(self,request):
-        data = request.data
-        serializer=TaskSerializer(data=data)
-        if not serializer.is_valid():
-            return Response({
-            "message" : "Data is not saved.",
-            "errors" : serializer.errors,        
-            })
+    return render(request,'index.html',context)
+
+#Update
+
+      
+def update(request,id):    #Support partial edits
+    queryset = Task.objects.get(id=id)
+
+    if request.method == "POST":
+        data = request.POST
+        title = data.get("title")
+        description = data.get("description")
+        task_type = data.get("task_type")
+    
+        queryset.title=title
+        queryset.description=description
+        queryset.task_type=task_type
+
+        queryset.save()
+        return redirect('/home')
+    
+    context = {'tasks' : queryset}
+    
+    return render(request,'update_task.html',context)
+
+
+#Delete
+
+def delete(request,id):
+            queryset = Task.objects.get(id=id)
+            queryset.delete()
+            return HttpResponse({
+                "Data Deleted."
+            }, status=status.HTTP_200_OK)
+ 
+
+
+
+
+# class TaskAPI(APIView):
+#     def get(self,request):
+#         queryset = Task.objects.all().order_by('-pk')
+#         serializer = TaskSerializer(queryset,many=True)
+
+#         return Response( serializer.data)
+    
+#     def post(self,request):
+#         data = request.data
+#         serializer=TaskSerializer(data=data)
+#         if not serializer.is_valid():
+#             return Response({
+#             "message" : "Data is not saved.",
+#             "errors" : serializer.errors,        
+#             })
         
-        serializer.save()
-        return Response({
-            "message" : "Data is saved."            
-        })
+#         serializer.save()
+#         return Response({
+#             "message" : "Data is saved."            
+#         })
 
     # def put(self,request):
     #     return Response({
@@ -42,48 +100,8 @@ class TaskAPI(APIView):
     #     })
     
 
-    # def patch(self,request):    #Support partial edits
-    #     data = request.data
-
-    #     if not data.get('id'):
-    #         return Response({
-    #             "message" : "Data is not uploaded.",
-    #             "errors" : "id is required", 
-    #         })
-        
-    #     task = Task.objects.get(id = data.get('id'))
-    #     serializer = TaskSerializer(task,data=data,partial=True)
-    #     if not serializer.is_valid():
-    #         return Response({
-    #         "message" : "Data is not saved.",
-    #         "errors" : serializer.errors,        
-    #         })
-        
-    #     serializer.save()
-    #     return Response({
-    #         "message" : "Data is saved."            
-    #     })
+    
 
 
-    def delete(self, request):
-        data = request.data
-
-        if not data.get('id'):
-            return Response({
-                "message": "Data is not deleted.",
-                "errors": "id is required"
-            }, status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            task = Task.objects.get(id=data.get('id'))
-            task.delete()
-            return Response({
-                "message": "Data Deleted.",
-                "data": {}
-            }, status=status.HTTP_200_OK)
-        except Task.DoesNotExist:
-            return Response({
-                "message": "Task not found.",
-                "errors": f"Task with id {data.get('id')} does not exist."
-            }, status=status.HTTP_404_NOT_FOUND)
+    
         
